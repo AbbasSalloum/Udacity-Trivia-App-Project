4d6d7f4c61c91c376350be7ed1cecc4d0e427a17
AbbasSalloum <salloum916@gmail.com>
2022-07-08 00:12:06 -0400

    endpoint to handle GET requests for questions

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -73,8 +73,20 @@ def create_app(test_config=None):
     @app.route('/question' , methods=['GET'])
     def retrieve_questions():
         selection=Question.query.order_by(Question.id).all()
-        current_question=paginate_questions(request,selection)
+        current_questions=paginate_questions(request,selection)
 
+        categories=Category.quesr.order_by(Category.type).all()
+
+        if len(current_questions)==0:
+            abort(404)
+
+        return jsonify({
+            'success':True,
+            'questions':current_questions,
+            'total_questions':len(selection)
+            'categories':{category.id:category,type for category in categories},
+            'current_category': None 
+        })
 
     """
     @TODO:
